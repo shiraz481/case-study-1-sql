397932850b41ea6c4f70d9948ab360de96c745b8
shiraz481 <99953755+shiraz481@users.noreply.github.com>
2022-03-02 20:07:27 +0200

    Delete Q1.sql

--- Q1.sql
+++
@@ -1,5 +0,0 @@
-#Which item was the most popular for each customer?
-select x.customer_id, x.product_id, max(x.num) from
-(SELECT customer_id, product_id, count(product_id) as num FROM dannys_diner.sales
-group by customer_id, product_id)x
-group by customer_id
